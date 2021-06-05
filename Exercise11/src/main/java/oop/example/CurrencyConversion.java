b10bac15fa2d774d130890b9c48f96f17425f7ae
jonathan-navarr <84359494+jonathan-navarr@users.noreply.github.com>
2021-06-05 14:58:55 -0400

    Add files via upload

---
+++ Exercise11/src/main/java/oop/example/CurrencyConversion.java
@@ -0,0 +1,21 @@
+package oop.example;
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Jonathan Navarro
+ */
+public class CurrencyConversion {
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in);
+        double euros, usdollars, exchangerate;
+        System.out.print("How many euros are you exchanging? ");
+        euros = scnr.nextInt();
+        System.out.print("What is the exchange rate? ");
+        exchangerate = scnr.nextDouble();
+        usdollars = ((euros*exchangerate) / 100);
+        String strUsDollars = String.format("%.2f", usdollars);
+        String strEuros = String.format("%.2f", euros);
+        System.out.print( strEuros + " euros at an exchange rate of "+ exchangerate + " is\n" + strUsDollars + " U.S. dollars.");
+
+    }
+}
